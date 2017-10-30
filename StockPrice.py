f9fbff9fb185ed9c8c0c8aa9b43f1ee2f96d148d
Rohan Gupta <rgupta@sevone.com>
2017-10-30 12:01:26 +0530

    Try-Except
    Adding the try except block

--- StockPrice.py
+++ StockPrice.py
@@ -5,25 +5,28 @@ import urllib.request
 
 arrayOfStocks = ['TCS','HDFC','RELIANCE','YES']
 
-for i in arrayOfStocks:
+try:
+    for i in arrayOfStocks:
 
-    url ="https://finance.google.com/finance?q="
-    #stock = "TATA"
-    url = url + i
-    #print(url)
-    urldata = urllib.request.urlopen(url).read()
-    urlDecodeData = urldata.decode("utf8")
-    m = re.search('meta itemprop="price"' , urlDecodeData)  # This will get you the position of the searched string the start and end positon
-    start = m.start()
-    end = start + 50
-    newString = urlDecodeData[start:end]
-    #print(newString)
-    m = re.search('content="', newString)
-    start = m.end()
-    newString1 = newString[start:]
+        url ="https://finance.google.com/finance?q="
+        #stock = "TATA"
+        url = url + i
+        #print(url)
+        urldata = urllib.request.urlopen(url).read()
+        urlDecodeData = urldata.decode("utf8")
+        m = re.search('meta itemprop="price"' , urlDecodeData)  # This will get you the position of the searched string the start and end positon
+        start = m.start()
+        end = start + 50
+        newString = urlDecodeData[start:end]
+        #print(newString)
+        m = re.search('content="', newString)
+        start = m.end()
+        newString1 = newString[start:]
 
-    m = re.search("/",newString1)
-    start = 0
-    end = m.end() - 3
-    final = newString1[0:end]
-    print("The value of " + i + " is " + final)
+        m = re.search("/",newString1)
+        start = 0
+        end = m.end() - 3
+        final = newString1[0:end]
+        print("The value of " + i + " is " + final)
+except:
+    print("Sorry we could not find your desired stock " + i + ".")
